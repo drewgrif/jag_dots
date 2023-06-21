5c566caa092d9850f5dbb95bba04c92b3557ce21
JustAGuyLinux <drew@drewgriffin.com>
2023-06-21 16:24:13 -0400

    Create autostart.sh

---
+++ .local/share/dwm/autostart.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+slstatus &
+# network applet
+
+nm-applet &
+
+# background
+feh --bg-scale ~/.config/backgrounds/debianpaint.png &
+
+# compositor
+picom --config ~/.config/picom/picom.conf &
+
+# sxhkd
+sxhkd -c ~/.config/suckless/sxhkd/sxhkdrc &
+
+# Notifications
+dunst &
+
+# volume
+volumeicon &
