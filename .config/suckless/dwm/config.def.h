00d87d42ab1eb8ea274b74f14afa1c51c121498e
JustAGuyLinux <drew@drewgriffin.com>
2024-02-13 12:36:27 -0500

    Update config.def.h

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -81,8 +81,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,            			 XK_Return,     spawn,                  {.v = termcmd } }, 
 /*	{ MODKEY,                       XK_b,      togglebar,      {0} }, */
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_Right,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Left,      focusstack,     {.i = -1 } },
 	{ MODKEY|ControlMask,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY|ControlMask,                       XK_d,      incnmaster,     {.i = -1 } }, 
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
