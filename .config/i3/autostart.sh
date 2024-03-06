cb4f267dfc4200048e81b1397b092e966615aee6
JustAGuyLinux <drew@drewgriffin.com>
2024-03-05 19:44:33 -0500

    slight changes to i3

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,14 +1,16 @@
 
 # Autostart applications
-# /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-# nitrogen --restore; sleep 1; 
+
+# bar start
 ~/bin/polybar-i3 &
-feh --bg-fill ~/.config/backgrounds/bluemountain.jpg &
+
+# wallpaper
+feh --bg-fill ~/.config/backgrounds/wallhaven-wewvxp_3440x1440.png &
+
+# compositor and notifications
 picom -b &
 numlockx on &
-# nm-applet &
-# volumeicon &
 dunst &
 # blueman-applet &
 
