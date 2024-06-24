0f336e8fdb3d8b0bb2d149c81bae15b404c68b09
JustAGuyLinux <drew@drewgriffin.com>
2024-06-24 08:51:11 -0400

    fixing i3 config

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -3,13 +3,13 @@
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
 # bar start
-~/bin/polybar-i3 &
+~/.config/i3/polybar-i3 &
 
 # wallpaper
 feh --bg-fill ~/.config/backgrounds/wallhaven-578rw7_3440x1440.png &
 
 # compositor and notifications
-picom -b &
+picom --animations -b &
 numlockx on &
 dunst &
 # blueman-applet &
