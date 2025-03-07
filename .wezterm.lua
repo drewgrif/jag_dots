12427c77582c85bbf7ed8162edb0aff4ea42e038
JustAGuyLinux <jag@justaguylinux.com>
2025-03-06 19:44:37 -0500

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,10 +6,20 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.window_background_opacity = 0.85  -- Adjust the value between 0 (fully transparent) and 1 (fully opaque)
+config.window_background_opacity = 0.90  -- Adjust the value between 0 (fully transparent) and 1 (fully opaque)
+
+-- config.front_end = "OpenGL"
+config.enable_wayland = false
+config.max_fps = 144
+config.default_cursor_style = "BlinkingUnderline"
+config.animation_fps = 1
+config.cursor_blink_rate = 500
+config.term = "xterm-256color" -- Set the terminal type
 
 -- For example, changing the color scheme:
-config.color_scheme = 'UnderTheSea'
+config.color_scheme = 'nightfox'
+
+config.use_fancy_tab_bar = true
 
 -- Changing the fontsize makes the windows launch in non-maximized mode.
 config.font_size = 16
@@ -17,7 +27,7 @@ config.font_size = 16
 config.font = wezterm.font('SauceCodePro Nerd Font Mono', { weight = 'Regular', italic = false })
 -- Font for the tabs
 config.window_frame = {
- font = wezterm.font { family = 'FiraCode Nerd Font Mono', weight = 'Bold' },
+ font = wezterm.font { family = 'JetBrainsMono Nerd Font Mono', weight = 'Regular' },
 }
 
 -- and finally, return the configuration to wezterm
